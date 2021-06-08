1c2b8bfa292b4548afc580030d5a40d5f812d2e4
homoluctus <w.slife18sy@gmail.com>
2021-06-08 15:14:58 +0900

    refactor(oauth): Separate interface expression and replace OAuth class to functions

--- src/oauth.ts
+++ src/oauth.ts
@@ -1,80 +1,64 @@
 import crypto from 'crypto';
 import { v4 as uuid } from 'uuid';
 import { camel2snake, rfc3986 } from './utils';
+import {
+  RequestQuery,
+  OAuthRequest,
+  OAuthSignatureSource,
+  OAuthSecret
+} from './interfaces';
 
-interface Query {
-  [name: string]: any;
+export function generate(
+  method: string,
+  baseUrl: string,
+  query: RequestQuery,
+  oauthConsumerKey: string,
+  oauthConsumerSecret: string,
+  oauthToken: string,
+  oauthTokenSecret: string,
+  oauthNonce: string = uuid().replace(/-/, ''),
+  oauthTimestamp: string = Math.floor(Date.now() / 1000).toString(),
+  oauthSignatureMethod: string = 'HMAC-SHA1',
+  oauthVersion: string = '1.0'
+): string {
+  const req = { method, baseUrl, query };
+  const source = {
+    oauthConsumerKey,
+    oauthToken,
+    oauthNonce,
+    oauthTimestamp,
+    oauthSignatureMethod,
+    oauthVersion
+  };
+  const secret = { oauthConsumerSecret, oauthTokenSecret };
+  const oauthSignature = sign(req, source, secret);
+  const entity = { ...source, oauthSignature };
+  return `OAuth ${concat(entity, '"', ', ')}`;
 }
 
-interface Entity {
-  [name: string]: string;
+function sign(
+  req: OAuthRequest,
+  source: OAuthSignatureSource,
+  secret: OAuthSecret
+): string {
+  const params = concat({ ...source, ...req.query }, '', '&');
+  const base = [req.method.toUpperCase(), req.baseUrl, params]
+    .map(rfc3986)
+    .join('&');
+  const key = Object.values(secret).map(rfc3986).join('&');
+  return crypto.createHmac('sha1', key).update(base).digest('base64');
 }
 
-export class OAuth {
-  oauthConsumerKey: string;
-  oauthToken: string;
-  oauthNonce: string;
-  oauthTimestamp: string;
-  oauthSignature: string;
-  oauthSignatureMethod: string = 'HMAC-SHA1';
-  oauthVersion: string = '1.0';
-
-  constructor(
-    method: string,
-    baseUrl: string,
-    query: Query,
-    oauthConsumerKey: string,
-    oauthConsumerSecret: string,
-    oauthToken: string,
-    oauthTokenSecret: string,
-    oauthNonce: string = '',
-    oauthTimestamp: string = ''
-  ) {
-    this.oauthConsumerKey = oauthConsumerKey;
-    this.oauthToken = oauthToken;
-    this.oauthNonce = oauthNonce || uuid().replace(/-/, '');
-    this.oauthTimestamp =
-      oauthTimestamp || Math.floor(Date.now() / 1000).toString();
-    this.oauthSignature = this.sign(
-      method,
-      baseUrl,
-      query,
-      oauthConsumerSecret,
-      oauthTokenSecret
+function concat(
+  obj: { [name: string]: string },
+  wrap: string,
+  sep: string
+): string {
+  const params: string[] = [];
+  Object.entries(obj)
+    .sort()
+    .forEach(([k, v]) =>
+      params.push(`${camel2snake(k)}=${wrap}${rfc3986(v)}${wrap}`)
     );
-  }
-
-  generate(): string {
-    return `OAuth ${this.concat(this.entity, '"', ', ')}`;
-  }
-
-  private get entity(): Entity {
-    const entity: Entity = {};
-    Object.entries(this).forEach(([k, v]) => (entity[k] = v));
-    return entity;
-  }
-
-  private sign(
-    method: string,
-    url: string,
-    query: Query,
-    oauthConsumerSecret: string,
-    oauthTokenSecret: string
-  ): string {
-    const params = this.concat({ ...this.entity, ...query }, '', '&');
-    const base = [method.toUpperCase(), url, params].map(rfc3986).join('&');
-    const key = [oauthConsumerSecret, oauthTokenSecret].map(rfc3986).join('&');
-
-    return crypto.createHmac('sha1', key).update(base).digest('base64');
-  }
-
-  private concat(obj: object, wrap: string, sep: string): string {
-    const params: string[] = [];
-    Object.entries(obj)
-      .sort()
-      .forEach(([k, v]) =>
-        params.push(`${camel2snake(k)}=${wrap}${rfc3986(v)}${wrap}`)
-      );
-    return params.join(sep);
-  }
+  return params.join(sep);
 }
